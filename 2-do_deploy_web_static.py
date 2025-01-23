f05b96a19a693423bba0e1cc840f0af9a7a02622
mohammedabarh <abmohammed606@gmail.com>
2025-01-23 12:55:50 -0800

    hn

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,12 @@
-script based on the file 1-pack_web_static.py that distributes an
+#!/usr/bin/python3
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['54.164.253.166', '52.204.144.35']
 
 
 def do_deploy(archive_path):
