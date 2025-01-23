a6b3272575392932f9a7e16f48eee91c2870524f
mohammedabarh <abmohammed606@gmail.com>
2025-01-23 09:52:36 -0800

    AirBnB_clone_v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-# 3-deploy_web_static.py
-
 from fabric.api import env
-from os.path import exists
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+from fabric.operations import local
+from fabric.contrib.files import exists
 
-env.hosts = ['<IP web-01>', '<IP web-02>']  # Replace with actual IPs
+env.hosts = ['<IP_web_01>', '<IP_web_02>']
 
 def deploy():
-    """Creates and distributes an archive to web servers."""
+    """Creates and distributes an archive to your web servers."""
     archive_path = do_pack()
     if not archive_path:
         return False
+
     return do_deploy(archive_path)
