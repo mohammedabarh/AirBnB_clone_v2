5c70337b340a3c8ce14ba54888a6b24f6b9e04d0
mohammedabarh <abmohammed606@gmail.com>
2025-01-23 09:12:16 -0800

    AirBnB_clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,10 @@ def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder."""
     if not os.path.exists("versions"):
         local("mkdir versions")
-    
+
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_path = f"versions/web_static_{timestamp}.tgz"
-    
+
     local(f"tar -cvzf {archive_path} web_static")
-    
+
     return archive_path if os.path.exists(archive_path) else None
