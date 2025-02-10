5e37c8418cf4d4aff2f1fb03faa0e3b63d2d9c6f
mohammedabarh <abmohammed606@gmail.com>
2025-02-10 04:07:41 +0000

    task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-""" this module contains """
-from models import *
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from flask import Flask, render_template
+"""Starts a Flask 
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ display HTML page with list of states """
-    states = storage.all(classes["State"]).values()
-    # ^ fetches States data from storage engine, then in line below,
-    # those states are passed into the template
-    return render_template('7-states_list.html', states=states)
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def remove_SQLalc_session(exception):
-    """ close storage when tear down is executed """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
